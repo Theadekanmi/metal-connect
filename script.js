aa87c7372fa19254c21f46aaf58897182c04697d
Theadekanmi <theadekanmi@gmail.com>
2024-12-01 12:34:43 -0800

    changes

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const navLink = document.getElementById("navLink");
 Menu.addEventListener("click",()=>{ 
 navLink.classList.toggle("hidden");
 });
+
+function setActiveLink(link) {
+    const links = document.querySelectorAll('.item a');
+    links.forEach((link) => {
+        link.classList.remove('active-link');
+    });
+    link.classList.add('active-link');
+}
