ea8cee1dba233a7a443000b65feb2e2fe0666101
Theadekanmi <theadekanmi@gmail.com>
2024-12-03 12:27:37 -0800

    changes

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const navLink = document.getElementById("navLink");
-Menu.addEventListener("click",()=>{ 
-navLink.classList.toggle("hidden");
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.item a');
+    const currentPath = window.location.pathname.split('/').pop();
+
+    links.forEach((link) => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active-link');
+        }
+
+        link.addEventListener('click', function() {
+            links.forEach((link) => {
+                link.classList.remove('active-link');
+            });
+            link.classList.add('active-link');
+        });
+    });
 });
 
-function setActiveLink(link) {
-    const links = document.querySelectorAll('.item a');
-    links.forEach((link) => {
-        link.classList.remove('active-link');
-    });
-    link.classList.add('active-link');
-}
+// Check TensorFlow.js is loaded
+console.log(tf.version.tfjs);  // This will log the version of TensorFlow.js
+
+
+
+
